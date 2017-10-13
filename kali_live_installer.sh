8cb722fdb06042eb2334b733a6d713e1f8aa9bb5
smartersec <amous3@mt2015.com>
2017-10-13 00:02:41 -0500

    Update kali_live_installer.sh

--- kali_live_installer.sh
+++ kali_live_installer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt-get update && apt-get install shadowsocks-libev encfs figlet surf speedtest-cli -y
+apt-get update && apt-get install shadowsocks-libev encfs figlet toilet surf speedtest-cli -y
 chmod +x patvpn-blackarch.py
 ln -s /root/PatriotVPN/patvpn-blackarch.py /usr/bin/patvpn
 echo 'socks5   127.0.0.1   1080' >> /etc/proxychains.conf
