3b702d8b41f714e8521a43fae9beb681066bc5a8
smartersec <amous3@mt2015.com>
2017-10-13 00:06:00 -0500

    Update blackarch_live_install.sh

--- blackarch_live_install.sh
+++ blackarch_live_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pacman -Sy && pacman -S shadowsocks-libev --noconfirm && pacman -S encfs --noconfirm && pacman -S figlet --noconfirm && pacman -S surf --noconfirm && pacman -S speedtest-cli --noconfirm
-chmod +x patvpn-blackarch.py
-ln -s /root/PatriotVPN/patvpn-blackarch.py /usr/bin/patvpn
+chmod +x patvpn-blackarch-kali-root.py
+ln -s /root/PatriotVPN/patvpn-blackarch-kali-root.py /usr/bin/patvpn
 echo 'socks5   127.0.0.1   1080' >> /etc/proxychains.conf
 patvpn
