da9b446da2a5d34d7731ce677614b6258816f84f
smartersec <amous3@mt2015.com>
2017-10-12 19:23:08 -0500

    Update blackarch_install.sh

--- blackarch_install.sh
+++ blackarch_install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-pacman -Sy && pacman -S encfs --noconfirm && pacman -S figlet --noconfirm && pacman -S surf --noconfirm && pacman -S speedtest-cli --noconfirm
+pacman -Sy && pacman -S shadowsocks-libev --noconfirm && pacman -S encfs --noconfirm && pacman -S figlet --noconfirm && pacman -S surf --noconfirm && pacman -S speedtest-cli --noconfirm
 chmod +x patvpn.py
-./patvpn.py
+ln -s /root/PatriotVPN/patvpn.py /usr/bin/patvpn
+patvpn
